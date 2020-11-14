47e6678378ef24d92804c6768307128ffc43e652
NikyParfenov <nikyparfenov@gmail.com>
2020-11-14 13:50:48 +0300

    'Pass up to 21.11.2020'

--- gbparsers/spiders/instagram.py
+++ gbparsers/spiders/instagram.py
@@ -2,6 +2,7 @@
 Instagram parser of the next data blocks:
 1. Parse of hashtags with associated posts (parse date, data, photos url with saving its on a disk)
 2. Parse of users followings and followers
+3. Handshake chain
 """
 import scrapy
 import json
@@ -23,11 +24,11 @@ class InstagramSpider(scrapy.Spider):
                   'followings': 'd04b0a864b4b54837c0d870b0e77e076',
                   }
 
-    def __init__(self, login, enc_password, *args, **kwargs):
+    def __init__(self, users, login, enc_password, *args, **kwargs):
         # tags for parsing
         self.tags = ['python', 'pythonprogramming']
         # part of users url for parsing of followings and followers
-        self.users_follow = ['gal_gadot', 'katyperry']
+        self.users_follow = users
         self.__login = login
         self.__enc_password = enc_password
         super().__init__(*args, **kwargs)
@@ -86,6 +87,92 @@ class InstagramSpider(scrapy.Spider):
                                      data=edge['node'],
                                      img=[edge['node']['display_url']],
                                      )
+
+    @staticmethod
+    def js_data_extract(response):
+        # extract data from a java script
+        script = response.xpath('//script[contains(text(), "window._sharedData =")]/text()').get()
+        return json.loads(script.replace('window._sharedData =', '')[:-1])
+
+    def users_follow_parse(self, response, user_page=None, query_variables=None):
+        if user_page is None:
+            user_page = self.js_data_extract(response)['entry_data']['ProfilePage'][0]['graphql']['user']
+        if query_variables is None:
+            query_variables = {"id": user_page['id'],
+                               "first": 100,
+                               }
+        for follow_type in self.follow_types.keys():
+            url = f'{self.pagination_url}?query_hash={self.query_hash[follow_type]}' \
+                  f'&variables={json.dumps(query_variables)}'
+            yield response.follow(url, callback=self.followings_parse, cb_kwargs={'user_page': user_page,
+                                                                                  'follow_type': follow_type,
+                                                                                  })
+
+    def followings_parse(self, response, user_page, follow_type):
+        js_data = response.json()['data']['user'][self.follow_types[follow_type]]
+        yield from self.follow_item(user_page, js_data['edges'], follow_type)
+        if js_data['page_info']['has_next_page']:
+            query_variables = {"id": user_page['id'],
+                               "first": 100,
+                               "after": js_data['page_info']['end_cursor'],
+                               }
+            yield from self.users_follow_parse(response, user_page, query_variables)
+
+    def follow_item(self, user_page, follow_users, follow_type):
+        for user in follow_users:
+            if follow_type == 'followings':
+                yield InstagramUserFollowItems(user_id=user_page['id'],
+                                               user_name=user_page['username'],
+                                               follow_id=user['node']['id'],
+                                               follow_name=user['node']['username'],
+                                               )
+            else:
+                yield InstagramUserFollowItems(user_id=user['node']['id'],
+                                               user_name=user['node']['username'],
+                                               follow_id=user_page['id'],
+                                               follow_name=user_page['username'],
+                                               )
+
+
+class InstagramHandshakeSpider(scrapy.Spider):
+    name = 'instagram'
+    allowed_domains = ['www.instagram.com']
+    start_urls = ['https://www.instagram.com/']
+    __login_url = 'https://www.instagram.com/accounts/login/ajax/'
+    pagination_url = '/graphql/query/'
+    follow_types = {'followings': 'edge_follow',
+                    'followers': 'edge_followed_by',
+                    }
+    query_hash = {'followers': 'c76146de99bb02f6415203be841dd25a',
+                  'followings': 'd04b0a864b4b54837c0d870b0e77e076',
+                  }
+
+    def __init__(self, users, login, enc_password, *args, **kwargs):
+        # part of users url for parsing of followings and followers
+        self.users_follow = users
+        self.__login = login
+        self.__enc_password = enc_password
+        super().__init__(*args, **kwargs)
+
+    def parse(self, response, **kwargs):
+        try:
+            js_data = self.js_data_extract(response)
+            yield scrapy.FormRequest(
+                self.__login_url,
+                method='POST',
+                callback=self.parse,
+                formdata={
+                    'username': self.__login,
+                    'enc_password': self.__enc_password,
+                },
+                headers={'X-CSRFToken': js_data['config']['csrf_token']},
+            )
+        except AttributeError as e:
+            if response.json().get('authenticated'):
+                for user in self.users_follow:
+                    yield response.follow(f'/{user}/',
+                                          callback=self.users_follow_parse,
+                                          )
 
     @staticmethod
     def js_data_extract(response):
